29d316ff1ebb0a86424f973f75ba5e83e74f1531
Gasti <gaston@spinomenal.com>
2018-05-06 08:41:10 +0300

    first commit

---
+++ scripts/modules/navbar.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+
+class Navbar {
+  constructor() {
+	this.menuIcon = $(".navbar__icon-box");
+	this.navbar = $(".navbar");
+
+    this.events();
+  }
+
+  events() {
+    this.menuIcon.click(this.toggleTheMenu.bind(this));
+  }
+
+  toggleTheMenu() {
+	this.menuIcon.toggleClass("active");
+	this.navbar.toggleClass("active");
+  }
+}
+
+export default Navbar;
